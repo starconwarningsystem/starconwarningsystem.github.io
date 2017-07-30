45b3820542e0075722a74d1f5c1947dafbdf01be
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-07-29 21:29:00 -0400

    Removed Obsolete Cache
    Whole window is the "background", no real content for now.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,58 @@
-var cacheName = 'starcon-step-6-1';var filesToCache = [  '/',  '/index.html',  '/index-old.html',  '/about.html',  '/colors.html',  '/forum.html',  '/messages.html',  '/messages-old.html',  '/rate-us.html',  '/files/main_style.css',  '/files/theme/external/font.css',  '/files/theme/external/sites.css',  '/files/theme/external/analytics.js',  '/files/theme/external/api.js',  '/files/theme/external/jquery.min.js',  '/files/theme/external/main.js',  '/files/theme/custom.js',  '/files/theme/plugins.js',  '/activateserviceworker.js',  '/files/theme/external/regular.ttf',  '/uploads/images/defcon-5.jpg'];self.addEventListener('install', function(e) {  console.log('[ServiceWorker] Install');  e.waitUntil(    caches.open(cacheName).then(function(cache) {      console.log('[ServiceWorker] Caching app shell');      return cache.addAll(filesToCache);    })  );});self.addEventListener('activate', function(e) {  console.log('[ServiceWorker] Activate');  e.waitUntil(    caches.keys().then(function(keyList) {      return Promise.all(keyList.map(function(key) {        if (key !== cacheName && key !== dataCacheName) {          console.log('[ServiceWorker] Removing old cache', key);          return caches.delete(key);        }      }));    })  );  return self.clients.claim();});self.addEventListener('fetch', function(e) {  console.log('[Service Worker] Fetch', e.request.url);  var dataUrl = 'https://starconwarningsystem.github.io';  if (e.request.url.indexOf(dataUrl) > -1) {    /*     * When the request URL contains dataUrl, the app is asking for fresh     * weather data. In this case, the service worker always goes to the     * network and then caches the response. This is called the "Cache then     * network" strategy:     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network     */    e.respondWith(      caches.open(dataCacheName).then(function(cache) {        return fetch(e.request).then(function(response){          cache.put(e.request.url, response.clone());          return response;        });      })    );  } else {    /*     * The app is asking for app shell files. In this scenario the app uses the     * "Cache, falling back to the network" offline strategy:     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network     */    e.respondWith(      caches.match(e.request).then(function(response) {        return response || fetch(e.request);      })    );  }});
+var cacheName = 'starcon-step-6-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/index-old.html',
+  '/about.html',
+  '/colors.html',
+  '/forum.html',
+  '/messages.html',
+  '/messages-old.html',
+  '/rate-us.html',
+  '/files/main_style.css',
+  '/files/theme/external/font.css',
+  '/files/theme/external/sites.css',
+  '/files/theme/external/analytics.js',
+  '/files/theme/external/api.js',
+  '/files/theme/external/jquery.min.js',
+  '/files/theme/external/main.js',
+  '/files/theme/custom.js',
+  '/files/theme/plugins.js',
+  '/activateserviceworker.js',
+  '/files/theme/external/regular.ttf',
+  '/uploads/images/defcon-5.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
