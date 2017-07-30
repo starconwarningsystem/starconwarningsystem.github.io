3865f39a079f688c4a5c0f18904e9a7cd9cfe0cc
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-07-29 21:34:41 -0400

    Offline Forums?

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,8 @@ var filesToCache = [
   '/files/theme/plugins.js',
   '/activateserviceworker.js',
   '/files/theme/external/regular.ttf',
-  '/uploads/images/defcon-5.jpg'
+  '/uploads/images/defcon-5.jpg',
+  '//www.powr.io/plugins/comments/cached_view?load=sync&index=0&unique_label=&powr_token=weebly_683304140623557845&user_label=weebly_683304140623557845_6c35bc94-d03a-4120-bbf5-a1b157607839&demo_mode=false&external_type=weebly-integrated&template_powr_token=&color=rgb(62%2C%2062%2C%2062)&backgroundColor=rgb(255%2C%20255%2C%20255)&fontFamily=%22Quattrocento%20Sans%22%2C%20sans-serif'
 ];
 
 self.addEventListener('install', function(e) {
