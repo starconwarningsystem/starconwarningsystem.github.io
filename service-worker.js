8ca2aa87f7cddf30678d3d4db7db015338ffb5a5
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-08-21 19:24:43 -0400

    Fixed Service Worker

--- service-worker.js
+++ service-worker.js
@@ -46,15 +46,6 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-
 self.addEventListener('activate', function(e) {
   var cacheWhitelist = ['STARCON-2017-08-21-19-15'];
   console.log('[ServiceWorker] Activate');
@@ -65,6 +56,15 @@ self.addEventListener('activate', function(e) {
           console.log('[ServiceWorker] Removing old cache', cacheName);
           return caches.delete(cacheName);
       }}))
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
