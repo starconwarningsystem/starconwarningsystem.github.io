901dfd788bfc7ac435ce9237c0f527c357e69734
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-08-21 18:55:23 -0400

    Undid Last Few Tests

--- service-worker.js
+++ service-worker.js
@@ -28,15 +28,7 @@ var urlToCache = [
   '//fonts.googleapis.com/css?family=Quattrocento:400,700&subset=latin,latin-ext',
   '//fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic&subset=latin,latin-ext/',
   '//fonts.googleapis.com/css?family=Actor&subset=latin,latin-ext',
-  '//www.powr.io/plugins/comments/cached_view?load=sync&index=0&unique_label=&powr_token=weebly_683304140623557845&user_label=weebly_683304140623557845_6c35bc94-d03a-4120-bbf5-a1b157607839&demo_mode=false&external_type=weebly-integrated&template_powr_token=&color=rgb(62%2C%2062%2C%2062)&backgroundColor=rgb(254%2C%20254%2C%20254)&fontFamily=%22Quattrocento%20Sans%22%2C%20sans-serif',
-  '//www.powr.io/powr.js',
-  '//js-agent.newrelic.com/nr-1044.min.js',
-  '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
-  '//maxcdn.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js',
-  '//www.powr.io/shims/styles/shim.css',
-  '//maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css',
-  '//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css',
-  '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
+  '//www.powr.io/plugins/comments/cached_view?load=sync&index=0&unique_label=&powr_token=weebly_683304140623557845&user_label=weebly_683304140623557845_6c35bc94-d03a-4120-bbf5-a1b157607839&demo_mode=false&external_type=weebly-integrated&template_powr_token=&color=rgb(62%2C%2062%2C%2062)&backgroundColor=rgb(254%2C%20254%2C%20254)&fontFamily=%22Quattrocento%20Sans%22%2C%20sans-serif'
 ];
 
 self.addEventListener('install', function(e) {
