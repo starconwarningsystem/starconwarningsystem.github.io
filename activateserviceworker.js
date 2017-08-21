ed18f0e2d40723395b40789aa0e88726ba9b702c
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-08-21 19:13:11 -0400

    Fixed Service Worker Activation

--- activateserviceworker.js
+++ activateserviceworker.js
@@ -1,9 +1,12 @@
 (function() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
         // Registration was successful
         console.log('Service Worker registered');
+      }, function(err) {
+        // registration failed :(
+        console.log('Service Worker registration failed: ', err);
       });
     });
   }
