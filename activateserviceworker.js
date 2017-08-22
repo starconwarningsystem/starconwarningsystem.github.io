a751bdb4ba10ada83c435bbf871b0aa0a218c2c0
The STARCON Warning System <starconwarningsystem@gmail.com>
2017-08-21 21:11:40 -0400

    I'm Done

--- activateserviceworker.js
+++ activateserviceworker.js
@@ -1,12 +1,7 @@
 (function() {
-if(navigator.onLine) {
+
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        for(let registration of registrations) {
-          registration.unregister();
-      }});
-      console.log('Service Worker unregistered');
       navigator.serviceWorker.register('/service-worker.js', {Cache: "max-age=0"}).then(function(registration) {
         // Registration was successful
         console.log('Service Worker registered');
@@ -16,5 +11,4 @@ if(navigator.onLine) {
       });
     });
   }
-}
 })();
